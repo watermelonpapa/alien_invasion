7c057a1eeb5138805d84ff0a20abd0b86eba4d4f
watermelonpapa <beyondjobs@qq.com>
2025-02-06 21:19:58 +0800

    add a ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,26 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230,230,230)
+        self.bg_color = self.settings.bg_color
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
+
             self.screen.fill(self.bg_color)
+            self.ship.blitme()
             pygame.display.flip()#双缓冲技术显示
 
 if __name__ == '__main__':#__name__是内置属性，看模块是否是按主程序运行。
