a4f7206acc2cfb2b559e52b4eddb3233ad97aea0
watermelonpapa <beyondjobs@qq.com>
2025-02-07 21:55:01 +0800

    left and don't beyond boader

--- ship.py
+++ ship.py
@@ -7,10 +7,14 @@ class Ship:
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         self.moving_right = False
+        self.moving_left = False
+        self.ship_speed = 2
         
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.ship_speed
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.rect.x -= self.ship_speed
 
     def blitme(self):
         self.screen.blit(self.image,self.rect)
